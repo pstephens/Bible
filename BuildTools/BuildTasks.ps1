d856b58d9df2fa0c2b8cc2d8ed51e6d1e1dddeed
Peter Stephens <peter.a.stephens@gmail.com>
2011-01-29 19:59:20 -0600

    Reworked automated testing back into the build script. modified:   BuildTools/BuildTasks.ps1

--- BuildTools/BuildTasks.ps1
+++ BuildTools/BuildTasks.ps1
@@ -13,7 +13,17 @@ Properties {
 
 Task default -depends UnitTests
 
-Task UnitTests -depends Build { Write-Host 'Unit Tests...' }
+Task UnitTests -depends Build { 
+	if(!(Test-Path $ViewRoot\Artifacts\BuildLogs))
+	{
+		New-Item -ItemType Directory $ViewRoot\Artifacts\BuildLogs
+	}
+	Exec { 
+		..\ExternalAssets\Nunit\nunit-console.exe `
+			$ViewRoot\Bible2.nunit `
+			/xml:$ViewRoot\Artifacts\BuildLogs\TestResult.xml 
+	}
+}
 
 Task Rebuild -depends Clean, Build
 
